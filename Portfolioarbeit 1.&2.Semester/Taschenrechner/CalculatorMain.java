ab9283d4f3929e30447e1aaaea04e9a50b22a640
manolo-monetha <66214395+manolo-monetha@users.noreply.github.com>
2020-05-31 19:21:02 +0200

    Create CalculatorMain.java

---
+++ Portfolioarbeit 1.&2.Semester/Taschenrechner/CalculatorMain.java
@@ -0,0 +1,11 @@
+public class CalculatorMain {
+	public static void main(String[] args) {
+		new Calculator(12)
+				.add(50)
+				.divide(4)
+				.multiply(2)
+				.subtract(2)
+				.multiply(3)
+				.printResult();
+	}
+}
