4efbef9d327b8a6c31682e7b7ba2d5ccb601d78f
manolo-monetha <66214395+manolo-monetha@users.noreply.github.com>
2020-05-31 19:19:41 +0200

    Create ICalculator.java

---
+++ Portfolioarbeit 1.&2.Semester/Taschenrechner/ICalculator.java
@@ -0,0 +1,11 @@
+public interface ICalculator {
+	public ICalculator add(double operand2);
+
+	public ICalculator subtract(double operand2);
+
+	public ICalculator multiply(double operand2);
+
+	public ICalculator divide(double operand2);
+
+	public void printResult();
+}
